e318411403d9badc6ba10058d3714b788308dbaa
Naveen Micheal <naveenmicheal@protonmail.com>
2018-11-15 12:11:37 +0530

    Added templates - Materialize css

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+# db = SQLAlchemy(app)
+
+# tables
+# class Url(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     longurl = db.Column(db.String(700), nullable=False)
+#     shorturl = db.Column(db.String(100), nullable=False)
+#     uniq = db.Column(db.String(100), nullable=True)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='3333')
+    app.run(host='0.0.0.0', port='3333', debug=True)
 
