751b577c88981f98513b2c0f0ee9e32c1656beae
Naveen Micheal <naveenmicheal@protonmail.com>
2018-12-18 23:02:28 +0530

    Long url add - Done, ID fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb .db'
 db = SQLAlchemy(app)
 
 class Url(db.Model):
+    __tablename__= 'url'
     id = db.Column(db.Integer, primary_key=True)
     longurl = db.Column(db.String(700), nullable=False)
     shorturl = db.Column(db.String(100), nullable=False)
@@ -16,13 +17,21 @@ class Url(db.Model):
         self.longurl= longurl
         self.shorturl=shorturl
         self.uniq = uniq
-
+# db.drop_all()
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method =='POST':
         lurl = request.form.get('lurl')
-        return lurl
-        
+        dbfile = open('db_id_file.txt','r')
+        lastid = str(dbfile.read())
+        lastid = int(lastid)
+        newid = lastid +1
+        dbfile = open('db_id_file.txt', 'w')
+        dbfile.write(str(newid))
+        item = Url(newid,lurl,'http://test.com/123', 'itisuniq')   
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('index'))
     else:    
         return render_template('home.html')
 
