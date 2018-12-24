3ba427bb1fdaebad367551da731568d08e415ea0
Naveen Micheal <naveenmicheal@protonmail.com>
2018-12-24 22:59:54 +0530

    Worked logging url and redirection with some dummy data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import base64, datetime, binascii, string, random
+import base64, datetime, binascii, string, random, requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb .db'
@@ -11,14 +11,41 @@ class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     longurl = db.Column(db.String(700), nullable=False)
     shorturl = db.Column(db.String(100), nullable=False)
-    uniq = db.Column(db.String(100), nullable=True, primary_key = True)
+    uniq = db.Column(db.String(100), nullable=False,unique=True, primary_key = True)
+    count =db.Column(db.Integer, nullable=False)
 
-    def __init__(self, id , longurl,shorturl,uniq):
+    def __init__(self, id , longurl,shorturl,uniq, count):
         self.id = id
         self.longurl= longurl
         self.shorturl=shorturl
         self.uniq = uniq
+        self.count = count
+
+class Data(db.Model):
+    __tablename__= 'data'
+    random_id = db.Column(db.String(100), primary_key =True)
+    longurl = db.Column(db.String(700), nullable=False)
+    shorturl = db.Column(db.String(100), nullable=False)
+    uniq = db.Column(db.String(100), nullable=False)
+    # useragent = db.Column(db.String(500), nullable=True)
+    browser = db.Column(db.String(50), nullable=True)
+    device = db.Column(db.String(50), nullable=True)
+    # location = db.Column(db.String(30), nullable=True)
+    isp = db.Column(db.String(30), nullable=True)
+
+    def __init__(self,random_id,longurl,shorturl,uniq,browser,device,isp):
+        self.random_id = random_id
+        self.longurl=longurl
+        self.shorturl = shorturl
+        self.uniq = uniq
+        # self.useragent = useragent
+        self.browser=browser
+        self.device = device   
+        # self.location = location  
+        self.isp = isp    
 # db.drop_all()
+# db.create_all()
+
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method =='POST':
@@ -49,14 +76,14 @@ def index():
         id_key = month + day + min + sec + r 
 
         try:
-            item = Url(id_key,lurl,'http://localhost:3333/'+uniqkey, uniqkey)
-            s_url = 'http://localhost:3333/'+uniqkey
-            print(s_url)   
+            item = Url(id_key,lurl,'http://localhost:3333/'+uniqkey, uniqkey, 0)
+            s_url = 'http://localhost:3333/'+uniqkey   
             db.session.add(item)
             db.session.commit()
+            return render_template('home.html', data = s_url)
         except:
             return redirect(url_for('dberrror'))    
-        return render_template('home.html', data = s_url)
+         
     else:    
         return render_template('home.html')
 
@@ -64,8 +91,59 @@ def index():
 def dberrror():
     return render_template('error.html')
  
+@app.route('/<s_url>')
+def surl(s_url):
+    # query to database
+    # try: 
+    print(s_url)
+    data = Url.query.filter_by(uniq = s_url).first()
+    # print('Before Count ' + str(data.count))
+    data.count = data.count+1
+     # print('After ' + str(data.count)) 
+    db.session.commit()
+    # For the DATA_URL table
+    raw_c = string.ascii_lowercase
+    raw_n = string.digits
+    random_id = ''
+    global random_id
+    # random_id = random.randint(0,9999999) / random.randint(1,5) * random.choice(raw_n)
+    random_id = str(random_id)
+    random_id =  random.choice(raw_c)+random.choice(raw_c)+random.choice(raw_c) +random.choice(raw_c) + random.choice(raw_c)+ random.choice(raw_c) + random.choice(raw_c) + random.choice(raw_c) 
 
+    longurl = data.longurl
+    shorturl =  data.shorturl
+    uniq = data.uniq
+    browser = request.user_agent.browser
+    #locaton updated after deployed
+    device = request.user_agent.platform
+    # location = 'Mars Near on Earth'
+    #isp updated after deployed
+    isp = 'Space X on Mars ISP'
+    # Add URL_DATA on Data table
 
+    item = Data(random_id,longurl,shorturl,uniq,browser,device,isp)
+    db.session.add(item)
+    db.session.commit()
+    return redirect(longurl)
+       
+        # addr = request.environ['REMOTE_ADDR']
+        # location = ''
+        # ispname = ''
+        # url = 'http://ip-api.com/json/8.8.8.8'
+        # isp_raw = requests.get(url)
+        # isp_raw = isp_raw.json()
+        # # print(isp_raw.text)
+        # status = isp_raw['status']
+        # print (status)
+        # if isp_raw['status'] == 'fail':
+        #         location = 'Not Available'
+        #         ispname = 'Not Available'
+        # else:
+        #         location = isp_raw['country']
+        #         ispname = isp_raw['isp']
+
+    # except:
+    #     return 'No record Found'
 @app.route('/about')
 def about():
     return render_template('about.html')
